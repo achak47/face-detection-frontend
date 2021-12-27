8a4988f169698e3fd9c9ce3f7a45c5fd6219f564
Your Name <you@example.com>
2021-12-27 16:09:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect,useRef } from 'react' ;
+import axios from 'axios';
+import './index.css';
+import Webcam from 'react-webcam' ;
+function App() {
+  const webcamRef = useRef(null) ;
+  const[name, setName] = useState('')
+  const videoConstraints = {
+    width : 200,
+    height : 200,
+    facingMode: 'user'
+  };
+  const capture = React.useCallback(
+    () => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      console.log(`imageSrc = ${imageSrc}`)
+                  //for deployment, you should put your backend url / api
+      axios.post('https://frozen-wave-24455.herokuapp.com/api', {data : imageSrc})
+          .then(res => {
+            //console.log(`response = ${res.data}`)
+            setName(res.data)
+      })
+          .catch(error => {
+            console.log(`error = ${error}`)
+      })
+    }, 
+     [webcamRef]
+    );
+  return (
+    <div>
+    <Webcam
+     audio = {false}
+	 height = {100}
+	 ref = {webcamRef}
+	 screenshotFormat = "image/jpeg"
+	 width = {150}
+	 videoConstraints = {videoConstraints}
+	/>
+    <button onClick={capture}>Click to Verify</button>
+	<h2>{name}</h2>
+  </div>
+  );
+}
+
+export default App;
